9c1a44d1b53fe6c01be223b19a3107164a8f9726
madhushri <madhushripatil@gmail.com>
2019-11-30 01:11:07 -0500

    knn

--- Project3/KNN/knn.py
+++ Project3/KNN/knn.py
@@ -8,63 +8,24 @@ class KNNClassifier:
 
     def __init__(self):
         self.config = None
+        self.k_fold = 0
         self.k = 0
-        self.train_attrs = []
-        self.train_class_labels = []
-        self.test_attrs = []
-        self.test_class_labels = []
-        self.predicted_class_labels = []
 
     def read_file(self, path):
         data = []
         with open(path) as fp:
             line = fp.readline()
+            pt = 1
             while line:
                 value = line.replace("\n", "").split("\t")
+                value.insert(0, pt)
+                pt += 1
                 data.append(value)
                 line = fp.readline()
+        data = np.array(data)
         return data
 
-    def process_data(self, data_points):
-        train_data_percent = self.config['training_percentage']
-        no_of_records = int(len(data_points) * (train_data_percent / 100))
-        print(no_of_records)
-
-        for i in range(len(data_points)):
-            feature_list = data_points[i]
-            if i < no_of_records:
-                self.train_class_labels.append(int(feature_list[len(feature_list) - 1]))
-            else:
-                self.test_class_labels.append(int(feature_list[len(feature_list) - 1]))
-            pts = []
-            for j in range(len(feature_list[0: len(feature_list) - 1])):
-                pts.append(float(feature_list[j]))
-
-            if i < no_of_records:
-                self.train_attrs.append(pts)
-            else:
-                self.test_attrs.append(pts)
-
-    # convert the string data to categorical
-
-    def pre_process(self, data):
-        # check are there any attributes which has string values
-        attributes_length = data.shape[1]
-        string_value_indexex = []
-        for i in range(0, attributes_length):
-            try:
-                float(data[0][i])
-            except ValueError:
-                string_value_indexex.append(i)
-
-        # got all the index attributes which are strings, now convert them to floats
-        for index in string_value_indexex:
-            data = self.convert_values_to_floats(data, index)
-
-        return data
-
-    # pre processing input features
-
+    # preprocessing input features
     def convert_values_to_floats(self, data, index):
         attribute_values = data[:, index]
         attribute_values = set(attribute_values)  # got unique values
@@ -82,46 +43,97 @@ class KNNClassifier:
 
         return data
 
-    def divide_data(self, data):
-        train_data_percent = self.config['training_percentage']
-        no_of_records = int(data.shape[0] * (train_data_percent / 100))
-        train_data = data[0:no_of_records]
-        test_data = data[no_of_records:]
-        return train_data, test_data
+    # convert the string data to categorical
+    def preprocess(self, data):
+        # check are there any attributes which has string values
+        attributes_length = data.shape[1]
+        string_value_indexex = []
+        for i in range(0, attributes_length):
+            try:
+                float(data[0][i])
+            except ValueError:
+                string_value_indexex.append(i)
 
-    def classify(self):
-        for i in range(len(self.test_attrs)):
+        # got all the index attributes which are strings, now convert them to floats
+        for index in string_value_indexex:
+            data = self.convert_values_to_floats(data, index)
+        return data
+
+    def euclidian_distance(self, x1, x2):  # it is used for calculating euclidean distance
+        distance = 0
+        for x in range(len(x1)):
+            distance += np.square(float(x1[x]) - float(x2[x]))
+        return np.sqrt(distance)
+
+    def process_and_classify_data(self, data):
+        rows = len(data)
+        cols = len(data[0])
+        data = self.preprocess(data)
+        data_split = self.divide_data(data, rows, cols)
+        test_data = []
+        accuracy = 0.0
+        precision = 0.0
+        recall = 0.0
+        f1_score = 0.0
+        predicted_labels = []
+        metric_calculator = MetricsCalculator()
+        for i in range(0, self.k_fold):
+            test_data = data_split[i]
+            train_data = []
+            for j in range(0, self.k_fold):
+                if j != i:
+                    for d in data_split[j]:
+                        train_data.append(d)
+
+            predicted_labels = self.classify(test_data, train_data)
+
+            accuracy += metric_calculator.calculate_accuracy(np.array(test_data), predicted_labels)
+            precision += metric_calculator.calculate_precision(np.array(test_data), predicted_labels)
+            recall += metric_calculator.calculate_recall(np.array(test_data), predicted_labels)
+
+        accuracy = float(accuracy / self.k_fold)
+        precision = float(precision / self.k_fold)
+        recall = float(recall / self.k_fold)
+        f1_score = metric_calculator.calculate_F1_score(precision, recall)
+
+        print("Accuracy: " + str(accuracy))
+        print("Precision: " + str(precision))
+        print("Recall: " + str(recall))
+        print("F1 Score: " + str(f1_score))
+
+    def divide_data(self, data, rows, cols):
+        no_of_records = int(rows / self.k_fold)
+        data_split = {}
+        for i in range(0, self.k_fold - 1):
+            start = i * no_of_records
+            end = start + no_of_records
+            data_split[i] = data[start:end]
+        data_split[i + 1] = data[end:]
+        return data_split
+
+    def classify(self, test_data, train_data):
+        predicted_class_labels = []
+        for i in range(len(test_data)):
             class0 = 0
             class1 = 0
             neighbors = {}
-            for j in range(len(self.train_attrs)):
-                neighbors[j] = distance = dist.euclidean(self.train_attrs[j], self.test_attrs[i])
+            for j in range(len(train_data)):
+                distance = self.euclidian_distance(test_data[i][1:len(test_data[i])-1], train_data[j][1:len(train_data[j])-1])
+                pts = [distance, train_data[j][0], int(train_data[j][len(train_data[j])-1])]
+                neighbors[j] = pts
 
             neighbors = sorted(neighbors.items(), key=lambda x: x[1])
-            for cnt in range(0, self.k):
-                val = int(neighbors[cnt][0])
-                if self.train_class_labels[val] == 0:
+            for cnt in range(self.k):
+                val = neighbors[cnt][1]
+                if val[2] == 0:
                     class0 = class0 + 1
                 else:
                     class1 = class1 + 1
             if class0 > class1:
-                self.predicted_class_labels.append(0)
+                predicted_class_labels.append(0)
             else:
-                self.predicted_class_labels.append(1)
-        print(len(self.predicted_class_labels))
-
-    def calculate_measures(self, data):
-        data = self.pre_process(np.array(data))
-        train_data_split, test_data_split = self.divide_data(data)
-        metric_calculator = MetricsCalculator()
-        accuracy = metric_calculator.calculate_accuracy(test_data_split, self.predicted_class_labels)
-        print("Accuracy: " + str(accuracy))
-        precision = metric_calculator.calculate_precision(test_data_split, self.predicted_class_labels)
-        print("Precision: ", precision)
-        recall = metric_calculator.calculate_recall(test_data_split, self.predicted_class_labels)
-        print("Recall: ", recall)
-        f1_score = metric_calculator.calculate_F1_score(precision, recall)
-        print("F1 Score: ", f1_score)
+                predicted_class_labels.append(1)
+        return predicted_class_labels
 
 
 def main():
@@ -130,10 +142,9 @@ def main():
         config = json.load(f)
     knn_classifier.config = config
     knn_classifier.k = knn_classifier.config['k']
+    knn_classifier.k_fold = knn_classifier.config['k_fold_validation']
     data = knn_classifier.read_file(knn_classifier.config['input_file'])
-    knn_classifier.process_data(data)
-    knn_classifier.classify()
-    knn_classifier.calculate_measures(data)
+    knn_classifier.process_and_classify_data(data)
 
 
 if __name__ == '__main__':
